707c5e26760675305c3b1fadd48f953edd553339
Rahulsaha1104 <138718573+Rahulsaha1104@users.noreply.github.com>
2023-07-29 22:55:21 +0530

    Add files via upload

---
+++ 29-NumberGame/src/com/aurionpro/test/GuessNumberGameTest.java
@@ -0,0 +1,10 @@
+package com.aurionpro.test;
+
+import com.aurionpro.model.NumberMethod;
+
+public class GuessNumberGameTest {
+	public static void main(String[] args) {
+		NumberMethod obj = new NumberMethod();
+		obj.startGame();
+	}
+}
