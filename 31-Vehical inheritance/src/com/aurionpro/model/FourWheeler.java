dd32b4f85e181fd02cfe6012e479bc66ce1017e5
Rahulsaha1104 <138718573+Rahulsaha1104@users.noreply.github.com>
2023-08-06 23:00:12 +0530

    Add files via upload

---
+++ 31-Vehical inheritance/src/com/aurionpro/model/FourWheeler.java
@@ -0,0 +1,13 @@
+package com.aurionpro.model;
+
+public class FourWheeler extends Vehicle {
+     protected double mileage;
+
+    public FourWheeler(String companyName, double mileage) {
+        super(companyName);
+        this.mileage = mileage;
+    }
+
+    
+}
+
